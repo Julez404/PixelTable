9ad3d73df4aa97d12778914e979ecc1097b5591c
Julian Linn <julian404@icloud.com>
2019-10-28 20:25:29 +0100

    Added inputCheck

--- python/setPixel.py
+++ python/setPixel.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 
+from validData import *
 import sys
 
+# Expected Input
+# 1: Row -> 0 to 9
+# 2: Column -> 0 to 19
+# 3: Color -> Hex (000000 to FFFFFF)
 
-f = open("/var/www/pixel/python/.exchange", "w+")
-f.write("setPixel" + '\n')
-f.write(sys.argv[1] + '\n')
-f.write(sys.argv[2] + '\n')
-f.write(sys.argv[3] + '\n')
-f.close()
+if (
+        isInt(sys.atgv[1]) and strLengthIs(sys.argv[1]),1) and
+        isInt(sys.atgv[2]) and (strLengthIs(sys.argv[2],1) or strLengthIs(sys.argv[2],1)) and
+        isHex(sys.atgv[3]) and strLengthIs(sys.argv[1],6)
+    ):
+    f = open("/var/www/pixel/python/.exchange", "w+")
+    f.write("setPixel" + '\n')
+    f.write(sys.argv[1] + '\n')
+    f.write(sys.argv[2] + '\n')
+    f.write(sys.argv[3] + '\n')
+    f.close()
+else:
+    f = open("/var/www/pixel/python/.error", "a")
+    f.write(sys.argv[1:] + '\n')
+    f.close()
