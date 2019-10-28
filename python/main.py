905488f60e04c5dc4622eef735f3109a5ba64d00
Julian Linn <julian404@icloud.com>
2019-10-28 14:46:11 +0100

    Moved setPixel to subroutine

--- python/main.py
+++ python/main.py
@@ -17,35 +17,36 @@ LED_COLLUMN_COUNT = 20
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ,LED_DMA,LED_INVERT,LED_BRIGHTNESS,LED_CHANNEL)
 strip.begin()
 
-
-for eachArg in sys.argv:
-	print (eachArg)
-
-def getPixelIndex(row,col):
-    return row*LED_COLLUMN_COUNT+col
-
-
 while True:
+	time.sleep(0.1)				# Delay Betwee Commands
 	f = open(".exchange","r")
 	input = f.readline()
 	f.close()
 
+for eachArg in sys.argv:
+	print (eachArg)
 	if input == ("setPixel"+'\n'):
-		f = open(".exchange","r")
-		f.readline()
-		row = f.readline()
-		column = f.readline()
-		color_hex = f.readline()
-		color_r = int( color_hex[0]+color_hex[1],16 )
-		color_g = int( color_hex[2]+color_hex[3],16 )
-		color_b = int( color_hex[4]+color_hex[5],16 )
-		index = getPixelIndex(int(row),int(column))
+		setPixel()
 
-		strip.setPixelColor(index,Color(color_r,color_g,color_b))
-		strip.show()
-		f.close()
-		f = open(".exchange","w")
-		f.close
-	time.sleep(0.1)
+def getPixelIndex(row,col):
+    return row*LED_COLLUMN_COUNT+col
 
+# Set single pixel in specific color
+def setPixel:
+	f = open(".exchange","r")
+	f.readline()
+
+	row = f.readline()
+	column = f.readline()
+	color_hex = f.readline()
+	color_r = int( color_hex[0]+color_hex[1],16 )
+	color_g = int( color_hex[2]+color_hex[3],16 )
+	color_b = int( color_hex[4]+color_hex[5],16 )
+	index = getPixelIndex(int(row),int(column))
+
+	strip.setPixelColor(index,Color(color_r,color_g,color_b))
+	strip.show()
+	f.close()
+	f = open(".exchange","w")
+	f.close
 
