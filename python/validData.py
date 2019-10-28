bb80f79a8859d37a5ac52f1f9eb9c10c3ece80b1
Julian Linn <julian404@icloud.com>
2019-10-28 17:55:32 +0100

    Added inputValidation script

---
+++ python/validData.py
@@ -0,0 +1,24 @@
+import sys
+
+# Check if number is a valid Hex Value
+def isHex(number):
+    try:
+        int(number,16)
+        return True
+    except:
+        return False
+
+# Check for valid integer
+def isInt(number):
+    try:
+        int(number,10)
+        return True
+    except:
+        return False
+
+# Check for expected str length
+def strLengthIs(str,size):
+    if (len(str) == size):
+        return True
+    else:
+        return False
