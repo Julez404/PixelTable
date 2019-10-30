0f33527a54e6067322228fab992cdeec4453b798
Thomas Dietrich <t.dietrich95@outlook.de>
2019-10-30 17:14:09 +0100

    Grid to an extern HTML file and Document import

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,23 +1,11 @@
 
 $(document).ready(function () {
-
-
     $("#First_Deck").load("/html/Cards/First_Deck.html");
     $("#Second_Deck").load("/html/Cards/Second_Deck.html");
     $("#Third_Deck").load("/html/Cards/Third_Deck.html");
-
     $("#Navbar_id").load("/html/Navs/Navbar_1.html");
     $("#sidebar").load("/html/Navs/Sidebar_1.html");
-
-
+    $("#GridCard1").load("/html/Cards/GridCard1.html");
 });
 
 
-$('#sidebar-btn').click(function () {
-    alert("hello");
-   
-    });
-
-$("body").click(function (event) {
-    $("#log").html("clicked: " + event.target.nodeName);
-});
