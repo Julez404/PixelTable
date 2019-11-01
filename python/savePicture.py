c831e95007a95e0cd4d932909ba7be7d02be5772
Julian Linn <julian404@icloud.com>
2019-11-01 10:46:06 +0100

    Command printout test

--- python/savePicture.py
+++ python/savePicture.py
@@ -7,12 +7,17 @@ import sys
 # Expected Input
 # 1: Name (isValidPictureName)
 
-if (isValidPictureName(sys.argv[1])):
     command = ["savePicture"]
     command.append(sys.argv[1])
     setNewCommand(command)
-else:
-    f = open("/var/www/pixel/python/.error", "a")
-    f.write(sys.argv[0] + '\n')
-    f.write(sys.argv[1] + '\n')
-    f.close()
+
+#
+#if (isValidPictureName(sys.argv[1])):
+#    command = ["savePicture"]
+#    command.append(sys.argv[1])
+#    setNewCommand(command)
+#else:
+#    f = open("/var/www/pixel/python/.error", "a")
+#    f.write(sys.argv[0] + '\n')
+#    f.write(sys.argv[1] + '\n')
+#    f.close()
