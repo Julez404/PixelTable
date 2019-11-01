60cd7aa7252f501297e54abcb92fa866a0b9d09f
Julian Linn <julian404@icloud.com>
2019-11-01 11:04:07 +0100

    Start Picture Debugging

--- python/main.py
+++ python/main.py
@@ -37,3 +37,75 @@ while True:
         PixelValuesToWeb(led_strip,command)
 #    if command[0] == ("savePicture"+'\n')
 #        savePicture(Test)
+
+
+EOF = ""
+# Commands
+# [1] Name
+
+# File = .savedPictures
+
+def nameIsAvailable(name):
+    f = open(".savedPictures", "r")
+    last_read = f.readline()
+    while last_read != EOF:
+        saved_name = last_read.lstrip("name=")
+        saved_name = last_read.rstrip('\n')
+
+        # Name taken -> Return false
+        if saved_name == name:
+            f.close()
+            return False
+        last_read = f.readline()
+        last_read = f.readline()
+    f.close()
+    return True
+
+def extractNames(data_array):
+    size = len(data_array)
+    name = ""
+    count = 0
+    while (count != size):
+        name.append(data_array[count]+"_")
+        count = count + 2;
+    return name;
+
+def savePicture(command):
+    # Check if name is already taken
+    if (nameIsAvailable(command[1])):
+        f = open(".savedPictures", "a")
+        f.write("name=" + command[1] + '\n')
+        f.write(getPixelValues())
+        f.close()
+
+def setPicture(command):
+    f = open(".savedPictures", "r")
+
+    last_read = f.readline()
+    while (last_read != EOF):
+        string_check = last_read.lstrip("name=")
+        string_check = last_read.rstrip('\n')
+
+        # Check if name is found
+        if string_check == command[1]:
+            led_values = f.readline()
+            readbackSet(led_values)
+            f.close()
+            break
+        last_read = f.readline()
+        last_read = f.readline()
+    f.close()
+
+def getAllPicturse(command):
+    f = open(".savedPictures", "r")
+    data = []
+    last_read = f.readline()
+    while(last_read != EOF):
+        data.append(last_read)
+        last_read = f.readline()
+    f.close()
+    allPictures = extractNames(data)
+    readbackSet(allPictures)
+
+def delPicture(command):
+    Do_Something = 0
