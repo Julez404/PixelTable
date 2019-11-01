9e342b422c5d82405edea50556a618c9f3bed8c4
Julian Linn <julian404@icloud.com>
2019-11-01 10:51:42 +0100

    Fixed PHP argument spelling, revert test case

--- python/savePicture.py
+++ python/savePicture.py
@@ -7,17 +7,12 @@ import sys
 # Expected Input
 # 1: Name (isValidPictureName)
 
+if (isValidPictureName(sys.argv[1])):
     command = ["savePicture"]
     command.append(sys.argv[1])
     setNewCommand(command)
-
-#
-#if (isValidPictureName(sys.argv[1])):
-#    command = ["savePicture"]
-#    command.append(sys.argv[1])
-#    setNewCommand(command)
-#else:
-#    f = open("/var/www/pixel/python/.error", "a")
-#    f.write(sys.argv[0] + '\n')
-#    f.write(sys.argv[1] + '\n')
-#    f.close()
+else:
+    f = open("/var/www/pixel/python/.error", "a")
+    f.write(sys.argv[0] + '\n')
+    f.write(sys.argv[1] + '\n')
+    f.close()
--- savePicture.php
+++ savePicture.php
@@ -1,6 +1,6 @@
 <?php
 
-$picture = $_GET['$picture'];
+$picture = $_GET['picture'];
 
 exec("./python/savePicture.py $picture",$output);
 print_r($output);
