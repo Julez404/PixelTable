6d341151cf8a34df5310953328eb511b6f837d91
Julian Linn <julian404@icloud.com>
2019-11-11 12:33:07 +0100

    Added "strip" to function calls

--- python/number.py
+++ python/number.py
@@ -7,205 +7,178 @@ Created on Sat Nov  9 13:24:28 2019
 from pixels import *
 from switchcase import switch
 
-    
-def setNumber(offset_x,offset_y,Number,farbe):
+
+def setNumber(strip,offset_x,offset_y,Number,farbe]):
     for case in switch(Number):
-        
+
         if case(0):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(5+offset_x,1+offset_y,farbe)
-            setPixel(6+offset_x,1+offset_y,farbe)
-            setPixel(7+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[5+offset_x,1+offset_y,farbe])
+            setPixel(strip,[6+offset_x,1+offset_y,farbe])
+            setPixel(strip,[7+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
             break
-        
+
         if case(1):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
             break
-        
+
         if case(2):
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(5+offset_x,1+offset_y,farbe)
-            setPixel(6+offset_x,1+offset_y,farbe)
-            setPixel(7+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[5+offset_x,1+offset_y,farbe])
+            setPixel(strip,[6+offset_x,1+offset_y,farbe])
+            setPixel(strip,[7+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
             break
-        
+
         if case(3):
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            break   
-        
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            break
+
         if case(4):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
             break
-        
+
         if case(5):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
             break
-        
+
         if case(6):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(5+offset_x,1+offset_y,farbe)
-            setPixel(5+offset_x,2+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,1+offset_y,farbe)
-            setPixel(7+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[5+offset_x,1+offset_y,farbe])
+            setPixel(strip,[5+offset_x,2+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,1+offset_y,farbe])
+            setPixel(strip,[7+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
             break
-        
+
         if case(7):
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
             break
-            
+
         if case(8):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(5+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,1+offset_y,farbe)
-            setPixel(7+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[5+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,1+offset_y,farbe])
+            setPixel(strip,[7+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
             break
-        
+
         if case(9):
-            setPixel(1+offset_x,3+offset_y,farbe)
-            setPixel(1+offset_x,2+offset_y,farbe)
-            setPixel(1+offset_x,1+offset_y,farbe)
-            setPixel(2+offset_x,1+offset_y,farbe)
-            setPixel(3+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,1+offset_y,farbe)
-            setPixel(4+offset_x,2+offset_y,farbe)
-            setPixel(5+offset_x,3+offset_y,farbe)
-            setPixel(8+offset_x,1+offset_y,farbe)
-            setPixel(8+offset_x,2+offset_y,farbe)
-            setPixel(8+offset_x,3+offset_y,farbe)
-            setPixel(7+offset_x,3+offset_y,farbe)
-            setPixel(6+offset_x,3+offset_y,farbe)
-            setPixel(2+offset_x,3+offset_y,farbe)
-            setPixel(3+offset_x,3+offset_y,farbe)
-            setPixel(4+offset_x,3+offset_y,farbe)
+            setPixel(strip,[1+offset_x,3+offset_y,farbe])
+            setPixel(strip,[1+offset_x,2+offset_y,farbe])
+            setPixel(strip,[1+offset_x,1+offset_y,farbe])
+            setPixel(strip,[2+offset_x,1+offset_y,farbe])
+            setPixel(strip,[3+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,1+offset_y,farbe])
+            setPixel(strip,[4+offset_x,2+offset_y,farbe])
+            setPixel(strip,[5+offset_x,3+offset_y,farbe])
+            setPixel(strip,[8+offset_x,1+offset_y,farbe])
+            setPixel(strip,[8+offset_x,2+offset_y,farbe])
+            setPixel(strip,[8+offset_x,3+offset_y,farbe])
+            setPixel(strip,[7+offset_x,3+offset_y,farbe])
+            setPixel(strip,[6+offset_x,3+offset_y,farbe])
+            setPixel(strip,[2+offset_x,3+offset_y,farbe])
+            setPixel(strip,[3+offset_x,3+offset_y,farbe])
+            setPixel(strip,[4+offset_x,3+offset_y,farbe])
             break
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
