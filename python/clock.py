eb73ea35a96176d0b4b73d851db224bf7fa92d8c
ina <inapenner1998@t-online.de>
2019-11-12 16:43:11 +0100

    add AlarmClock

--- python/clock.py
+++ python/clock.py
@@ -184,3 +184,154 @@ def showTimer(first_number_hours, second_number_hours,
                 setNumber(strip, 0, 15, compare_ziffer_4, 000000)
                 setNumber(strip, 0, 15, ziffer_4, parameters[1])
                 compare_ziffer_4 = ziffer_4
+               
+    
+    
+    
+    
+def showAlarmClock(time_hours, time_minutes, strip, parameters):          
+    #set Doppelpunkt
+        Offset_x = 0
+        Offset_y = 0
+        ziffer = 10
+        setNumber(strip, Offset_x, Offset_y, ziffer, parameters[1])
+    
+    #set erste Ziffer
+        Offset_x = 0
+        Offset_y = 0
+        zahl = int(time_hours)
+        ziffer_1 = getNumberOne(zahl)
+        setNumber(strip, Offset_x, Offset_y, ziffer_1, parameters[1])
+    
+    #set zweite Ziffer
+        Offset_x = 0
+        Offset_y = 4
+        zahl = zahl - (ziffer_1 * 10)
+        ziffer_2 = getNumberTwo(zahl)   
+        setNumber(strip, Offset_x, Offset_y, ziffer_2, parameters[1])
+    
+    #set dritte Ziffer
+        Offset_x = 0
+        Offset_y = 11
+        zahl = time_minutes
+        ziffer_3 = getNumberOne(zahl)
+        setNumber(strip, Offset_x, Offset_y, ziffer_3, parameters[1])
+    
+    #set vierte Ziffer
+        Offset_x = 0
+        Offset_y = 15
+
+        zahl = zahl - (ziffer_3 * 10)
+        ziffer_4 = getNumberTwo(zahl)
+        setNumber(strip, Offset_x, Offset_y, ziffer_4, parameters[1])
+        
+    
+def showStopWatch(strip, parameters):
+    ziffer_1 = 0
+    ziffer_2 = 0
+    ziffer_3 = 0
+    ziffer_4 = 0
+    ziffer_5 = 0
+    ziffer_6 = 0
+        
+    compare_ziffer_1 = 0
+    compare_ziffer_2 = 0
+    compare_ziffer_3 = 0
+    compare_ziffer_4 = 0
+    compare_ziffer_5 = 0
+    compare_ziffer_6 = 0
+    
+    
+    setNumber(strip, 0, 0, 10, parameters[1])
+    setNumber(strip, 0, 0, ziffer_1, parameters[1])
+    setNumber(strip, 0, 4, ziffer_2, parameters[1])
+    setNumber(strip, 0, 11, ziffer_3, parameters[1])
+    setNumber(strip, 0, 15, ziffer_4, parameters[1])
+    
+    
+    while True:
+        time.sleep(1)
+        
+        if(ziffer_6 < 9):
+            ziffer_6 = ziffer_6 + 1
+            
+        elif(ziffer_6 == 9):
+            ziffer_6 = 0
+            
+            if(ziffer_5 < 5):
+                ziffer_5 = ziffer_5 + 1
+            
+            elif(ziffer_5 == 5):
+                ziffer_5 = 0
+            
+                if(ziffer_4 < 9):
+                    ziffer_4 = ziffer_4 + 1
+            
+                elif(ziffer_4 == 9):
+                    ziffer_4 = 0
+                    
+                    if(ziffer_3 < 5):
+                        ziffer_3 = ziffer_3 + 1
+                        
+                    elif(ziffer_3 == 5):
+                        ziffer_3 = 0
+                        
+                        if(ziffer_2 < 9):
+                            ziffer_2 = ziffer_2 + 1
+                            
+                        elif(ziffer_2 == 9):
+                            ziffer_2 = 0
+                            
+                            if(ziffer_1 < 9):
+                                ziffer_1 = ziffer_1 + 1
+                                
+                            else:
+                                break
+        
+        
+        if((ziffer_1 == 0) and (ziffer_2 == 0)):
+           
+            setNumber(strip, 0, 0, 10, parameters[1])
+            if(compare_ziffer_3 != ziffer_3):
+                setNumber(strip, 0, 0, compare_ziffer_3, 000000)
+                setNumber(strip, 0, 0, ziffer_3, parameters[1])
+                compare_ziffer_3 = ziffer_3 
+            if(compare_ziffer_4 != ziffer_4):
+                setNumber(strip, 0, 4, compare_ziffer_4, 000000)
+                setNumber(strip, 0, 4, ziffer_4, parameters[1])
+                compare_ziffer_4 = ziffer_4  
+            if(compare_ziffer_5 != ziffer_5):
+                setNumber(strip, 0, 11, compare_ziffer_5, 000000)
+                setNumber(strip, 0, 11, ziffer_5, parameters[1])
+                compare_ziffer_5 = ziffer_5
+            if(compare_ziffer_6 != ziffer_6):
+                setNumber(strip, 0, 15, compare_ziffer_6, 000000)
+                setNumber(strip, 0, 15, ziffer_6, parameters[1])
+                compare_ziffer_6 = ziffer_6
+    
+             
+        else: 
+            setNumber(0, 0, 10, parameters[1])
+            if(compare_ziffer_1 != ziffer_1):
+                setNumber(strip, 0, 0, compare_ziffer_1, 000000)
+                setNumber(strip, 0, 0, ziffer_1, parameters[1])
+                compare_ziffer_1 = ziffer_1 
+            if(compare_ziffer_2 != ziffer_2):
+                setNumber(strip, 0, 4, compare_ziffer_2, 000000)
+                setNumber(strip, 0, 4, ziffer_2, parameters[1])
+                compare_ziffer_2 = ziffer_2
+            if(compare_ziffer_3 != ziffer_3):
+                setNumber(strip, 0, 11, compare_ziffer_3, 000000)
+                setNumber(strip, 0, 11, ziffer_3, parameters[1])
+                compare_ziffer_3 = ziffer_3
+            if(compare_ziffer_4 != ziffer_4):
+                setNumber(strip, 0, 15, compare_ziffer_4, 000000)
+                setNumber(strip, 0, 15, ziffer_4, parameters[1])
+                compare_ziffer_4 = ziffer_4
+    
+    
+
+
+
+    
+    
